48b4510a4dbd074aad0cee0f606da32f26d9e145
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-03-19 17:57:37 -0400

    commit tree 01a9b70ae7bb938ec33a984f03c18c53dc5fb405

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ActiveRecord::Base
-  
+  # a review belongs to a game
+  #   def game
+  #     Game.find(self.game_id)
+  #   end
+  belongs_to :game
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,24 @@
-puts "🌱 Seeding data..."
+puts '🌱 Seeding data...'
 
 # run a loop 50 times
 50.times do
   # create a game with random data
-  game = Game.create(
-    title: Faker::Game.title,
-    genre: Faker::Game.genre,
-    platform: Faker::Game.platform,
-    price: rand(0..60) # random number between 0 and 60
-  )
+  game =
+    Game.create(
+      title: Faker::Game.title,
+      genre: Faker::Game.genre,
+      platform: Faker::Game.platform,
+      price: rand(0..60), # random number between 0 and 60
+    )
 
   # create between 1 and 5 reviews for each game
   rand(1..5).times do
     Review.create(
       score: rand(1..10),
       comment: Faker::Lorem.sentence,
-      game_id: game.id # use the ID (primary key) of the game as the foreign key
+      game_id: game.id, # use the ID (primary key) of the game as the foreign key
     )
   end
 end
 
-puts "🌱 Done seeding!"
+puts '🌱 Done seeding!'
